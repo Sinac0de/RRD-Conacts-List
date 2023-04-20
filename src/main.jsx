0be744b972e73bbf176edb7bb70adb981d013913
Sina Moradian <sinacsharp@gmail.com>
2023-04-20 15:15:53 +0330

    Add Contact route to the router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
 import "./index.css";
+import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
   },
+  { path: "/contacts/:contactId", element: <Contact /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
