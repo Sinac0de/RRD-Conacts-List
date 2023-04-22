36dbe17f738816550c73f2a816f38c03595e1649
Sina Moradian <sinacsharp@gmail.com>
2023-04-22 17:10:12 +0330

    Add editAction

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
-import EditContact from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 
 import Root, {
   loader as rootLoader,
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
